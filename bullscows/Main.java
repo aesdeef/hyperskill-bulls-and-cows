6a3e93c1a7318f783127e6b5133eaa39cf76c2d1
Miłosz Meller <git@milosz.me>
2025-03-15 17:55:01 +0100

    Finish Stage 2

--- bullscows/Main.java
+++ bullscows/Main.java
@@ -1,7 +1,47 @@
 package bullscows;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String code = "9305";
+        int bulls = 0;
+        int cows = 0;
+
+        String guess = scanner.nextLine().trim();
+
+        for (char digit : "0123456789".toCharArray()) {
+            int occurrencesInCode = 0;
+            int occurrencesInGuess = 0;
+            for (int i = 0; i < code.length(); i++) {
+                if (code.charAt(i) == digit && guess.charAt(i) == digit) {
+                    bulls++;
+                } else if (code.charAt(i) == digit) {
+                    occurrencesInCode++;
+                } else if (guess.charAt(i) == digit) {
+                    occurrencesInGuess++;
+                }
+            }
+            cows += Math.min(occurrencesInCode, occurrencesInGuess);
+        }
+
+        System.out.print("Grade: ");
+        if (bulls == 0 && cows == 0) {
+            System.out.println("None");
+        } else if (bulls == 0) {
+            String plural = cows > 1 ? "s" : "";
+            System.out.println(cows + " cow" + plural + ".");
+        } else if (cows == 0) {
+            String plural = bulls > 1 ? "s" : "";
+            System.out.println(bulls + " bull" + plural + ".");
+        } else {
+            String pluralBulls = bulls > 1 ? "s" : "";
+            String pluralCows = cows > 1 ? "s" : "";
+            System.out.println(bulls + " bulls" + pluralBulls + " and " + cows + " cow" + pluralCows + ".");
+        }
+        System.out.println("The secret code is " + code);
+        /*
         System.out.println("The secret code is prepared: ****.");
         System.out.println();
         System.out.println("Turn 1. Answer:");
@@ -32,5 +72,6 @@ public class Main {
         System.out.println("9305");
         System.out.println("Grade: 4 bulls.");
         System.out.println("Congrats! The secret code is 9305.");
+         */
     }
 }
