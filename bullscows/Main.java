48fd1fdda482ab78ec208cffdae9e087abca3d7d
Miłosz Meller <git@milosz.me>
2025-03-15 17:00:56 +0100

    Finish Stage 1

---
+++ bullscows/Main.java
@@ -0,0 +1,36 @@
+package bullscows;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The secret code is prepared: ****.");
+        System.out.println();
+        System.out.println("Turn 1. Answer:");
+        System.out.println("1234");
+        System.out.println("Grade: 1 cow.");
+        System.out.println();
+        System.out.println("Turn 2. Answer:");
+        System.out.println("5678");
+        System.out.println("Grade: 1 cow.");
+        System.out.println();
+        System.out.println("Turn 3. Answer:");
+        System.out.println("9012");
+        System.out.println("Grade: 1 bull and 1 cow.");
+        System.out.println();
+        System.out.println("Turn 4. Answer:");
+        System.out.println("9087");
+        System.out.println("Grade: 1 bull and 1 cow.");
+        System.out.println();
+        System.out.println("Turn 5. Answer:");
+        System.out.println("1087");
+        System.out.println("Grade: 1 cow.");
+        System.out.println();
+        System.out.println("Turn 6. Answer:");
+        System.out.println("9205");
+        System.out.println("Grade: 3 bulls.");
+        System.out.println();
+        System.out.println("Turn 7. Answer:");
+        System.out.println("9305");
+        System.out.println("Grade: 4 bulls.");
+        System.out.println("Congrats! The secret code is 9305.");
+    }
+}
